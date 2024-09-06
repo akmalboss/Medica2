b3cac6b52f12894e046458a475f630ebc7589f1f
Akmal <mamatovakmaljon@gmail.com>
2024-09-06 15:23:27 +0500

    Comment1

---
+++ app/src/main/java/uz/medica/medica2/IntroActivity.kt
@@ -0,0 +1,20 @@
+package uz.medica.medica2
+
+import android.content.Intent
+import android.os.Bundle
+import uz.medica.medica2.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            startBtn.setOnClickListener {
+                startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+            }
+        }
+    }
+}
