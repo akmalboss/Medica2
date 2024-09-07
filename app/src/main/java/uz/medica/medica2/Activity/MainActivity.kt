48a54a6b4ba1f72d7e69c26597dbe123b932f34b
Akmal <mamatovakmaljon@gmail.com>
2024-09-08 00:34:53 +0500

    Comment4

--- app/src/main/java/uz/medica/medica2/Activity/MainActivity.kt
+++ app/src/main/java/uz/medica/medica2/Activity/MainActivity.kt
@@ -1,14 +1,53 @@
 package uz.medica.medica2.Activity
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import uz.medica.medica2.Adapter.SliderAdapter
+import uz.medica.medica2.Model.SliderModel
+import uz.medica.medica2.ViewModel.MainViewModel
 import uz.medica.medica2.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel=MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initBanner()
+
+    }
+
+    private fun banners(image: List<SliderModel>) {
+        binding.viewPager2.adapter = SliderAdapter(image, binding.viewPager2)
+        binding.viewPager2.clipToPadding = false
+        binding.viewPager2.clipChildren = false
+        binding.viewPager2.offscreenPageLimit = 3
+        binding.viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.viewPager2.setPageTransformer(compositePageTransformer)
+
+        if (image.size > 1) {
+            binding.dotIndicator.visibility = View.VISIBLE
+            binding.dotIndicator.attachTo(binding.viewPager2)
+        }
+    }
+
+    private fun initBanner(){
+        binding.progressBarSlider.visibility=View.VISIBLE
+        viewModel.banners.observe(this, Observer {
+            banners(it)
+            binding.progressBarSlider.visibility = View.GONE
+        })
+        viewModel.loadBanners()
     }
 }
